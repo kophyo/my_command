a6a4ce61c6a5c675ef3cd09d33d3287f0e7afc91
Phyo <kophyo48@gmail.com>
2016-02-08 22:32:37 +0000

    Initial commit with my_command to test executable command

---
+++ lib/my_command.rb
@@ -0,0 +1,9 @@
+require "my_command/version"
+
+module MyCommand
+  class CatApi
+    def say_hello
+      puts 'Hello world'
+    end
+  end
+end
---
+++ lib/my_command/version.rb
@@ -0,0 +1,3 @@
+module MyCommand
+  VERSION = "0.1.0"
+end
