57f7e9d09b3cf26fb00ef6bbdd9bc69316699c8e
Phyo <kophyo48@gmail.com>
2016-02-09 01:03:30 +0000

    Make sure the class works with nil argument passed in. Refactor the CatApi abit. Added an invalid error message when invalid argument is passed in

--- lib/my_command.rb
+++ lib/my_command.rb
@@ -7,28 +7,31 @@ module MyCommand
     include HTTParty
     attr_reader :command
 
-    def initialize(command = 'browser')
-      @command = command
+    def initialize(command = nil)
+      @command = command || 'browser'
     end
 
     def run
-      if @command == 'browser'
+      case command
+      when 'browser'
         response = HTTParty.get('http://thecatapi.com/api/images/get?format=xml&type=jpg,png')
         image_url = response['response']['data']['images']['image']['url']
         Launchy.open(image_url)
-      elsif @command == 'file'
+      when 'file'
         File.open(cat_image_file, "wb") do |f|
           f.binmode
           f.write HTTParty.get('http://thecatapi.com/api/images/get?type=jpg').parsed_response
         end
-      elsif @command == 'fact'
+      when 'fact'
         response = JSON.parse(HTTParty.get('http://catfacts-api.appspot.com/api/facts').parsed_response)
         puts response['facts'].first
-      elsif @command == 'save_facts'
+      when 'save_facts'
         response = JSON.parse(HTTParty.get('http://catfacts-api.appspot.com/api/facts?number=100').parsed_response)
         File.open(cat_facts_file, "wb") do |f|
           f.write response['facts'].each_with_index.map{|fact, i| "#{i+1}. #{fact}"}.join("\n")
         end
+      else
+        puts "Invalid argument. Please use either browser, file, fact or save_facts"
       end
     end
 
