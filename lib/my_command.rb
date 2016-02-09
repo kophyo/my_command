594a6aa02b8d3cf1ea9b9e9f2ffe6fd3fcdfe1a3
Phyo <kophyo48@gmail.com>
2016-02-09 00:22:47 +0000

    file command added with spec.

--- lib/my_command.rb
+++ lib/my_command.rb
@@ -5,6 +5,7 @@ require 'httparty'
 module MyCommand
   class CatApi
     include HTTParty
+    attr_reader :command
 
     def initialize(command = 'browser')
       @command = command
@@ -15,7 +16,16 @@ module MyCommand
         response = HTTParty.get('http://thecatapi.com/api/images/get?format=xml&type=jpg,png')
         image_url = response['response']['data']['images']['image']['url']
         Launchy.open(image_url)
+      elsif @command == 'file'
+        File.open(desktop_image_file, "wb") do |f|
+          f.binmode
+          f.write HTTParty.get('http://thecatapi.com/api/images/get?type=jpg').parsed_response
+        end
       end
+    end
+
+    def desktop_image_file
+      File.join(Dir.home, 'Desktop', 'cat_image.jpg')
     end
   end
 end
--- spec/cat_api_spec.rb
+++ spec/cat_api_spec.rb
@@ -4,7 +4,7 @@ describe MyCommand::CatApi do
   describe '#run' do
     subject { MyCommand::CatApi.new(command)}
     context 'browse command' do
-      let(:command) { 'browse' }
+      let(:command) { 'browser' }
       let(:response) do
         {
           'response' => {
@@ -24,6 +24,26 @@ describe MyCommand::CatApi do
 
         expect(Launchy).to receive(:open).with("some_url")
         subject.run
+      end
+
+      it 'sets browse as default command if no command is passed in' do
+        expect(MyCommand::CatApi.new.command).to eq('browser')
+      end
+    end
+
+    context 'file command' do
+      let(:command) { 'file' }
+      let(:response) do
+        double(parsed_response: 'Hello World')
+      end
+
+      let(:desktop_image_file) { File.join('/tmp', 'test.txt') }
+
+      it 'save on the desktop an image of a cat, as an image file' do
+        allow(subject).to receive(:desktop_image_file).and_return(desktop_image_file)
+        allow(HTTParty).to receive(:get).with('http://thecatapi.com/api/images/get?type=jpg').and_return(response)
+        subject.run
+        expect(File.read(desktop_image_file)).to eq('Hello World')
       end
     end
   end
